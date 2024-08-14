74387ea136c15bd8e70ff9bb9196ef6e6d6d6a99
Victor Dumbrava <victordumbrava91@gmail.com>
2024-08-14 17:52:34 +0300

    Implement chatbot button

--- portfolio-website/src/components/Bot.jsx
+++ portfolio-website/src/components/Bot.jsx
@@ -1,3 +1,7 @@
 export default function Bot() {
-    return <div>Bot</div>
+    return (
+        <div className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-cyan-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce">
+            <ion-icon name="chatbox-ellipses"></ion-icon>
+        </div>
+    )
 }
